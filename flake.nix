1cfd69d9b5f04f469ce734f6b9a5124ae8f7292f
Stephen Huan <stephen.huan@cgdct.moe>
2025-12-22 02:21:03 -0500

    add z3 comment

--- flake.nix
+++ flake.nix
@@ -103,6 +103,7 @@
           meta = meta // { mainProgram = "verus"; };
         };
 
+        # EXPECTED_Z3_VERSION in tools/common/consts.rs
         z3 = pkgs.z3.overrideAttrs (finalAttrs: previousAttrs: {
           version = "4.12.5";
           src = pkgs.fetchFromGitHub {
